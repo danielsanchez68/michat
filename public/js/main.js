57901269182883e9484f7e22ee9802deb4d5b043
danielsanchez68 <danielsanchez68@hotmail.com>
2023-11-29 11:23:12 -0300

    v0.0.1

---
+++ public/js/main.js
@@ -0,0 +1,34 @@
+console.log('App Chat Websocket Client')
+
+const socket = io.connect()
+
+socket.on('mensajes', mensajes => {
+    console.log(mensajes)
+    render(mensajes)
+})
+
+function render(mensajes) {
+    const html = mensajes.map( mensaje => `
+        <div>
+            <b>${mensaje.autor}</b>
+            <i>${mensaje.texto}</i>
+        </div>
+    `).join(' ')
+
+    document.getElementById('mensajes').innerHTML = html
+}
+
+
+function addMensaje(e) {
+    e.preventDefault()
+
+    console.log('addMensaje')
+
+    const mensaje = {
+        autor: document.getElementById('autor').value,
+        texto: document.getElementById('texto').value,
+    }
+    socket.emit('nuevo-mensaje', mensaje)
+}
+
+document.querySelector('form').addEventListener('submit', addMensaje)
